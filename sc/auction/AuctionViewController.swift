842571036c69ffd746b317d5c9944fcd8cc0d942
Mac <mac@macdemacbook-pro.local>
2018-08-19 14:54:56 +0800

    add

---
+++ sc/auction/AuctionViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AuctionViewController.swift
+//  sc
+//
+//  Created by Mac on 2018/8/15.
+//  Copyright © 2018年 ajmc. All rights reserved.
+//
+
+import UIKit
+
+class AuctionViewController: UIViewController {
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
